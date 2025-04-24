f5e07725d560e7707cc3f83f6e2dd5aa660b3b97
DDKROFF <ddkroff@gmail.com>
2025-04-24 09:02:11 +0300

    Готов слайдер на второй странице

---
+++ js/index.js
@@ -0,0 +1,19 @@
+const swiper = new Swiper('.swiper', {
+
+    loop: true,
+  
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+  new Swiper('.swiper', {
+    Loop: true,
+    slidesPerView: 1,
+    spaceBetween: 0
+});
