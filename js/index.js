0289054e453743c031b4e5227f3a484b20d61389
DDKROFF <ddkroff@gmail.com>
2025-04-24 10:15:54 +0300

    Сделал оценку по звездам

--- js/index.js
+++ js/index.js
@@ -17,3 +17,41 @@ const swiper = new Swiper('.swiper', {
     slidesPerView: 1,
     spaceBetween: 0
 });
+
+window.addEventListener('orientationchange', () => {
+  swiper.update();
+  swiper.onResize();
+});
+
+//Отзыв через звезды//
+
+const stars = document.querySelectorAll('.star');
+let selectedRating = 0;
+
+stars.forEach(star => {
+  star.addEventListener('mouseover', () => {
+    const val = parseInt(star.getAttribute('data-value'));
+    highlightStars(val);
+  });
+
+  star.addEventListener('mouseout', () => {
+    highlightStars(selectedRating);
+  });
+
+  star.addEventListener('click', () => {
+    selectedRating = parseInt(star.getAttribute('data-value'));
+    highlightStars(selectedRating);
+  });
+});
+
+function highlightStars(rating) {
+  stars.forEach(star => {
+    const starVal = parseInt(star.getAttribute('data-value'));
+    if (starVal <= rating) {
+      star.classList.add('selected');
+    } else {
+      star.classList.remove('selected');
+    }
+  });
+}
+
